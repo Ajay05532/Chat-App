0b042023b925fec6cb039b35f4424c493159c94a
Ajay singh <115156392+Ajay05532@users.noreply.github.com>
2025-08-31 17:57:53 +0530

    Modified Chat Box

--- client/src/components/chatBar.jsx
+++ client/src/components/chatBar.jsx
@@ -14,8 +14,31 @@ const chatBar = () => {
             <img className='h-10' src={assets.help_icon} alt="menu" />
         </div>
         <div>
+    {/* This the chat here from the sender side and reciever side*/ }
+    <div className='flex-1 overflow-y-auto p-4 space-y-4'>
+        
+          <div  className='flex justify-end'>
+            <div className='flex items-end gap-2 max-w-[70%] flex-row-reverse' >
+              {/* Avatar */}
+              <img 
+                className='h-8 w-8 rounded-full object-cover' 
+                src={assets.profile_img} 
+                alt="avatar" 
+              />
+              
+              {/* Message Bubble */}
+              <div className='flex flex-col  items-end'>
+                <div className='px-4 py-2 rounded-2xl bg-blue-500 text-white rounded-br-md'>
+                  <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nihil.</p>
+                </div>
+                <span className='text-xs text-gray-500 mt-1'>12:10 AM</span>
+              </div>
+            </div> 
+          </div>
+      </div>
+
             <div className='absolute bottom-4 left-6 flex items-center bg-white w-[90%] justify-between rounded-md p-2' >
-                <input className='w-[85%]' type="text" placeholder='send a message'/>
+                <input className='w-[85%] outline-none' type="text" placeholder='send a message'/>
                 <div className='flex gap-3 items-center'>
                     <input type="file" id="image" accept='image/png, image/jpeg' hidden/>
                     <label htmlFor="image">
